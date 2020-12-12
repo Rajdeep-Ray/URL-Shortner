5fc31ff44b099d959f4a4e429498c10027c460e3
Rajdeep Ray <rayrajdeep2001@gmail.com>
2020-12-12 21:19:17 +0530

    Update

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,25 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const shortid = require('shortid');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+const db = admin.firestore();
+
+const app = express()
+
+app.post('/api', (req, res) => {
+    var uid = shortid.generate()
+    const myData = {
+        shortid: uid,
+        url: req.body.url
+    }
+    // eslint-disable-next-line promise/always-return
+    db.collection('urls').doc(uid).set(myData).then(() => {
+        res.json(myData);
+
+    }).catch((err) => console.log(err))
+
+})
+
+exports.url_short = functions.https.onRequest(app);
